78b0a84c9161e21419df38c85ed6f40cec08465a
TheodorBenga <theodorbenga@yahoo.com>
2022-05-31 18:18:52 +0200

    changes scene4

---
+++ Space Station VR/Unity project/Assets/ScriptsTheo/OperateComputerScene4.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+public class OperateComputerScene4 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject half_key;
+    [SerializeField]
+    private GameObject card;
+    [SerializeField]
+    private Text screenText;
+    [SerializeField]
+    private Material screenMat;
+    [SerializeField]
+    private Color startingColor;
+    [SerializeField]
+    private GameObject leftHand;
+    [SerializeField]
+    private GameObject rightHand;
+
+
+    private InteractableFunctionality interFunc;
+
+    private bool fired = false;
+    private bool keyIntroduced = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interFunc = FindObjectOfType<InteractableFunctionality>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //this works 
+        if (!keyIntroduced)
+        {
+            if (other.gameObject.tag == "PCCard" && (rightHand.GetComponent<Hand>().currentAttachedObject == card ||
+                                                    leftHand.GetComponent<Hand>().currentAttachedObject == card))
+            {
+                GameObject hand = GetTheHand(other.gameObject);
+
+                hand.GetComponent<Hand>().DetachObject(card);
+
+                card.SetActive(false);
+                keyIntroduced = true;
+
+
+            }
+        }
+        else
+        {
+            if (other.gameObject.tag == "Fire" && !fired)
+            {
+                interFunc.DeactivateRigidbodyConstraints(half_key);
+                //Do something with the key, move it to the user
+                //half_key.GetComponent<Rigidbody>().AddForce(-5f, 0, 0);
+                Debug.Log("Force added 1");
+                fired = true;
+            }
+        }
+    }
+
+
+    private GameObject GetTheHand(GameObject card)
+    {
+        if (Vector3.Distance(card.transform.position, leftHand.transform.position) < Vector3.Distance(card.transform.position, rightHand.transform.position))
+        {
+            return leftHand;
+        }
+        else
+        {
+            return rightHand;
+        }
+    }
+
+    private IEnumerator changeScreen()
+    {
+        Color baseColor = screenMat.color;
+        Color white = Color.white;
+
+        while (keyIntroduced == false)
+        {
+            GetComponent<AudioSource>().Play();
+            screenMat.color = baseColor;
+            yield return new WaitForSeconds(0.1f);
+            screenMat.color = white;
+            yield return new WaitForSeconds(0.05f);
+            screenMat.color = baseColor;
+            yield return new WaitForSeconds(0.1f);
+            screenMat.color = white;
+
+            yield return new WaitForSeconds(Random.Range(3, 10));
+        }
+        StartCoroutine(fadeText());
+    }
+
+    private IEnumerator fadeText()
+    {
+        while (screenText.GetComponent<CanvasGroup>().alpha > 0)
+        {
+            screenText.GetComponent<CanvasGroup>().alpha -= 0.02f;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
